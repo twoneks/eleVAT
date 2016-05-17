d36d781efc8b40191af5c4322bc346e2360ad597
simoneromanelli <simone.romanelli.sr@gmail.com>
2016-05-17 14:14:59 +0200

    created configuration class

--- lib/eleVAT.rb
+++ lib/eleVAT.rb
@@ -1,15 +1,22 @@
 require "eleVAT/version"
 
 module EleVAT
-  class << self
-    attr_accessor :configuration
-  end
-
-  def self.configuration
-    @configuration ||= Configuration.new
-  end
-
   def self.configure
-    yield(configuration)
+    @config ||= Configuration.new
+    yield(@config) if block_given?
+    @config
+  end
+
+  def self.config
+    @config || configure
+  end
+
+  class Configuration
+    attr_accessor :basic_tax_rate, :import_tax
+
+    def initialize
+      @basic_tax_rate = 10
+      @import_tax = 5
+    end
   end
 end
--- spec/eleVAT_spec.rb
+++ spec/eleVAT_spec.rb
@@ -1,18 +1,25 @@
 require "spec_helper"
 
 describe EleVAT do
+  describe "#configuration default values" do
+    it "has default basic_tax_rate set to 10" do
+      puts EleVAT.config
+      basic_tax_rate = EleVAT.config.basic_tax_rate
+      expect(basic_tax_rate).to eq(10)
+    end
+  end
   describe "#configure" do
     before do
       EleVAT.configure do |config|
-        config.basic_tax_rate = 10
+        config.basic_tax_rate = 22
       end
     end
 
     it "return the basic_tax_rate as an Integer" do
-      basic_tax_rate = EleVAT.basic_tax_rate
+      basic_tax_rate = EleVAT.config.basic_tax_rate
 
       expect(basic_tax_rate).to be_a(Integer)
-      expect(basic_tax_rate).to eq(10)
+      expect(basic_tax_rate).to eq(22)
     end
   end
 end
